6a5a3c3283a85924f1cc379c52acc3e4ce435a88
Gustavo Valmaña <gustavo.shotokai@gmail.com>
2024-02-05 12:00:04 -0500

    first commit

---
+++ src/app/entities/CarItem.ts
@@ -0,0 +1,6 @@
+import { Product } from "./Product";
+
+export interface CarItem {
+  products: Product[];
+  quantity: number;
+}
---
+++ src/app/entities/CarOrder.ts
@@ -0,0 +1,8 @@
+export interface CarOrder {
+  id: number;
+  client_name: string;
+  client_phone: string;
+  client_email: string;
+  date: Date;
+  total: number;
+}
